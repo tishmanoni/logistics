36a164ed7d890f044a9bef1ffd36ffc4b14ccc3f
tishmanoni <66375712+tishmanoni@users.noreply.github.com>
2023-04-12 16:24:38 +0100

    Database file edited

--- config/database.php
+++ config/database.php
@@ -2,6 +2,22 @@
 
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
+define('DB_SERVER', 'tishmandb-instance-1.cwkmdpagwisw.us-east-1.rds.amazonaws.com');
+define('DB_USERNAME', 'admin');
+define('DB_PASSWORD', 'England10$');
+define('DB_NAME', 'cargo');
+
+/* Attempt to connect to MySQL database */
+$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+
+// Check connection
+if($link === false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
+}
+?>
+
+<!-- /* Database credentials. Assuming you are running MySQL
+server with default setting (user 'root' with no password) */
 define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
@@ -13,5 +29,4 @@ $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
 // Check connection
 if($link === false){
     die("ERROR: Could not connect. " . mysqli_connect_error());
-}
-?>
+} -->
